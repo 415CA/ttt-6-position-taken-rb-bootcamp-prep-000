402def7c351d25a4d5a5463957b3ccf5f29317c1
Chris Allen <chrisallen415@gmail.com>
2020-05-17 13:23:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   position = board[index - 1]
   if position == "" || position == " " || position == nil
     return false
-  else position == "X" || position == "O"
+  elsif position == "X" || position == "O"
     return true
   end
 end
